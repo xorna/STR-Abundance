a59dd158a8970e1efc0bc53faba3fd341891cca6
Tejas Joglekar <67784373+xorna@users.noreply.github.com>
2024-01-09 19:39:38 +0530

    rep_abundance_chr.py

--- rep_abundance_chr.py
+++ rep_abundance_chr.py
@@ -9,50 +9,27 @@ sys.path.append('D:/ACTREC/Poster')
 import common.common_functions as common_functions
 
 
-# used to take PERF file and GENOME file as command-line arguments
-# try:
-#
-#     # sys.argv is global, so any changes to it will reflect elsewhere in the code.
-#     # take sys.argv into a local-scoped variable for use
-#     args = sys.argv
-#     perf_file = args[1]
-#     genome_file = args[2]
-#     output_file = args[3]
-#
-#     print(f"Command-line options provided: \n"
-#           f"\t {perf_file} as PERF file \n"
-#           f"\t {genome_file} as Genome file \n"
-#           f"\t {output_file} as output JSON \n")
-#
-# except IndexError:
-#
-#     # in case of missing argument, this will be printed to STDOUT
-#     raise SystemExit(f"Usage: {sys.argv[0]} <PERF file> <Genome file> <Output json file>")
+used to take PERF file and GENOME file as command-line arguments
+try:
 
-# reading perf tsv file perf_data = common_functions.import_tsv(
-# file="/home/gourab/Public/Tejas_2023/Perf_Runs/Human_ref/GRCh38.p14_perf_annotation.tsv") perf_data =
-# common_functions.import_tsv(file="/home/gourab/Public/Tejas_2023/Perf_Runs/Human_ref/T2T-CHM13v2.0_perf_annotation
-# .tsv")
+    # sys.argv is global, so any changes to it will reflect elsewhere in the code.
+    # take sys.argv into a local-scoped variable for use
+    args = sys.argv
+    perf_file = args[1]
+    genome_file = args[2]
+    output_file = args[3]
 
-# for chromosome, chr_start in chromosomes_list.items():
-#
-#     chr_list, chr_base_count = common_functions.chromosome_parser(chromosome=chromosome, fasta=file_path)
-#
-#     print("Running Repeat Counter \n")
-#     rep_count_list = common_functions.repeat_counter(valid_row_num=row_number,
-#                                                      data_frame=perf_data,
-#                                                      curr_chr=chromosome,
-#                                                      repeat_data=chromosomes_list)
-#
-#     print("Saving results")
-#     common_functions.repeat_abundance(repeat_count_list=rep_count_list,
-#                                       raw_base_counts=chr_base_count,
-#                                       tsv_file="D:/ACTREC/Poster/PrimaryAnalysis/Results/RefGenome/GRCh38.p14",
-#                                       chr_name=chromosome)
+    print(f"Command-line options provided: \n"
+          f"\t {perf_file} as PERF file \n"
+          f"\t {genome_file} as Genome file \n"
+          f"\t {output_file} as output JSON \n")
+
+except IndexError:
+
+    # in case of missing argument, this will be printed to STDOUT
+    raise SystemExit(f"Usage: {sys.argv[0]} <PERF file> <Genome file> <Output json file>")
 
 def run(chromosome):
-
-    file_path = "D:/ACTREC/Review Paper/HumanRefGenome/GRCh38.p14/GenomeFasta/GCF_000001405.40_GRCh38.p14_genomic.fna"
 
     chr_list, chr_base_count = common_functions.chromosome_parser(chromosome=chromosome, fasta=file_path)
 
